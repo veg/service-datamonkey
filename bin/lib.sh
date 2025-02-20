82dcf4e3879bdec5e66fdbc2be595a8d81281b91
Danielle Callan <dmleake92@gmail.com>
2025-02-20 11:33:11 -0500

    go-gin-server generated code

--- bin/lib.sh
+++ bin/lib.sh
@@ -7,12 +7,14 @@ function manageDeps {
 
 function getApiSpec {
   echo "Pulling down latest OpenAPI Specification"
-  wget https://raw.githubusercontent.com/d-callan/api-datamonkey/refs/heads/master/dist/openapi.yaml
+  wget https://raw.githubusercontent.com/d-callan/api-datamonkey/refs/heads/master/dist/openapi.yaml -O openapi.yaml 
   echo "OpenAPI Specification retrieved!"
 }
 
 function generateServer {
+  echo "Starting server code generation"
   npx @openapitools/openapi-generator-cli generate -i openapi.yaml -g go-gin-server -o . --skip-validate-spec
+  echo "Code generation complete"
 }
 
 if [ "$#" -ge 2 ]; then
